5159d45f47259b2b7564d860ae1ff649832707ba
Hoitoo 999 <ramonanicoleta1999@gmail.com>
2025-08-25 17:19:05 +0300

    Add playGamefunction to play 5 rounds and declare a winner

--- script.js
+++ script.js
@@ -16,3 +16,43 @@ function gtHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  } else {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  }
+}
+
+function playGame() {
+  humanScore = 0;
+  computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
+
+  console.log(`Final Score - You: ${humanScore}, Computer: ${computerScore}`);
+  if (humanScore > computerScore) {
+    console.log("🎉 You win the game!");
+  } else if (humanScore < computerScore) {
+    console.log("😢 You lose the game!");
+  } else {
+    console.log("🤝 It's a tie game!");
+  }
+}
+
+playGame();
